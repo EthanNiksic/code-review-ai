c061dc54a292c25ead2183c906b8f245eb85650a
EthanNiksic <ethanniksic123@gmail.com>
2026-08-15 01:50:01 -0500

    Parse GitHub PR URL into API endpoint

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string url = args[0];
+string[] parts = url.Split('/');
+
+string owner = parts[3];
+string repo = parts[4];
+string number = parts[6];
+
+string apiUrl = $"https://api.github.com/repos/{owner}/{repo}/pulls/{number}";
+
+Console.WriteLine("Owner:  " + owner);
+Console.WriteLine("Repo:   " + repo);
+Console.WriteLine("Number: " + number);
+Console.WriteLine("API:    " + apiUrl);
